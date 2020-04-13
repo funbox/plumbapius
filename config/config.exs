9584f4d292b739ec8d52a9e349fd764b4f0afd33
a.kireev <a.kireev@fun-box.ru>
2020-04-13 14:06:56 +0200

    [IDEV-1270] убрал sentry config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,3 @@
 import Config
 
-config :sentry,
-  dsn:
-    "http://210d058342a64f9e92b84548489188ed:25a16e545e764ae58a4101343bf567f9@sentry.funbox.ru/90",
-  included_environments: [Mix.env()],
-  environment_name: Mix.env(),
-  enable_source_code_context: true,
-  tags: %{"service" => "plumbapius"}
-
 import_config "#{Mix.env()}.exs"
