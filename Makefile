ae8c654f82f9c65914cd0c50c49da9849e84fcdb
m.malkin <mmalkin@fun-box.ru>
2020-04-20 15:38:48 +0300

    [IDEV-1280] настроил публикацию в hex

--- Makefile
+++ Makefile
@@ -9,3 +9,8 @@ test:
 	mix credo --strict
 	mix cover
 	mix cover.lint
+
+hex-publish:
+	env HEX_API_URL="https://hex.funbox.ru/api/repos/funbox" \
+		HEX_API_KEY="$$(ghettoauth show-credentials -c fb-corptools-prod -r hex_user -b)" \
+		mix hex.publish package
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plumbapius.MixProject do
     [
       app: :plumbapius,
       version: "0.1.0",
-      elixir: "~> 1.10.0-otp-22",
+      elixir: "~> 1.10.0",
       deps: deps(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -19,36 +19,43 @@ defmodule Plumbapius.MixProject do
         arch_test: :test
       ],
       elixirc_paths: elixirc_paths(Mix.env()),
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      package: package(),
+      description: "Plumbapius"
+    ]
+  end
+
+  def package do
+    [
+      name: "plumbapius",
+      licenses: ["proprietary"],
+      links: %{funbox: "https://bb.funbox.ru/projects/APIB/repos/plumbapius/browse"}
     ]
   end
 
   defp elixirc_paths(env) when env in [:test], do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:jason, "~> 1.2"},
       {:ex_json_schema, "~> 0.7.3"},
+      {:plug, "~> 1.0"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.11", only: :test},
-      {:excoveralls_linter, "~> 0.0.2", only: :test},
-      {:plug, "~> 1.0"}
+      {:excoveralls_linter, "~> 0.0.2", only: :test}
     ]
   end
 
   defp aliases do
     [
-      test: ["test --no-start"],
       cover: ["coveralls --sort cov:desc --umbrella"],
       "cover.lint": ["coveralls.lint --missed-lines-threshold=2 --required-file-coverage=0.9"],
       "cover.html": ["coveralls.html --umbrella"],
