bd5f6b597417b5a28d7e9abb9bb3ef7b020c3f10
m.malkin <mmalkin@fun-box.ru>
2020-08-04 14:46:26 +0300

    Version bumpup: 0.11.0 → 0.12.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plumbapius.MixProject do
   def project do
     [
       app: :plumbapius,
-      version: "0.11.0",
+      version: "0.12.0",
       elixir: "~> 1.10.0",
       deps: deps(),
       start_permanent: Mix.env() == :prod,
